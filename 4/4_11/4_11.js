bf6c515f34a4b0e5d1bc9ca5089ed2eb8b47e448
[shotauchi] <[tianneixiang84@gmail.com]>
2024-01-12 17:48:03 +0000

    javascript-11

---
+++ 4/4_11/4_11.js
@@ -0,0 +1,10 @@
+let a = 'mom';
+if (a > 'mom') {
+    console.log('a=' + a + 'January'); 
+} else if (a < 0) {
+    console.log('a=' + a + 'February'); 
+} else if (a === 0) {
+    console.log('a=' + a + 'March'); 
+} else {
+    console.log('a=' + a + 'April');    
+}
