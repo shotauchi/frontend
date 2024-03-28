dce0ed0868e95c9e505099ba55913b255d21688b
[shotauchi] <[tianneixiang84@gmail.com]>
2024-03-28 19:22:43 +0000

    javascript-18

--- 3/3-18/3_18.js
+++ 3/3-18/3_18.js
@@ -3,3 +3,4 @@
 let inputValue = prompt();  // 入力ダイアログボックスから値を取得
 let x = inputValue * 110 / 100;     // 入力の値に消費税10%を加算して、xに代入
 alert(x);                   // xの値を警告ダイアログボックスに表示
+//消費税を加えてからinputvalueと×こと。
--- 4/4_11/4_11.js
+++ 4/4_11/4_11.js
@@ -2,11 +2,11 @@ let a = 5;
 let b = 8;
 
 if (a >= b) {
-    console.log('January'); 
+    console.log('January'); //aがｂ以上ならばJanuaryと出力される。
 } else if (a > b) {
-    console.log('February'); 
+    console.log('February'); //aがｂよりも大きいならばFabruaryと出力される。
 } else if (a < b) {
-    console.log('March'); 
+    console.log('March'); //aがｂよりも小さいならばMarchと出力される。
 } else {
-    console.log('April');    
+    console.log('April'); //それ以外ならばAprilが出力される。   
 }
