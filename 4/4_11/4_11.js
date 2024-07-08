56de2100d199fb0661b898f2566be63c2d67fe10
[shotauchi] <[tianneixiang84@gmail.com]>
2024-07-08 02:08:41 +0000

    最新をコミット

--- 4/4_11/4_11.js
+++ 4/4_11/4_11.js
@@ -1,12 +1,26 @@
-let a = 5;
-let b = 8;
+let a = 5;// 月
+let b = 8;// 日？
 
 if (a >= b) {
-    console.log('January'); //aがｂ以上ならばJanuaryと出力される。
+    console.log('January'); //３月が２９日以上ならばJanuaryと出力される。
 } else if (a > b) {
-    console.log('February'); //aがｂよりも大きいならばFabruaryと出力される。
+    console.log('February'); //３月が２９日よりも大きいならばFabruaryと出力される。
 } else if (a < b) {
-    console.log('March'); //aがｂよりも小さいならばMarchと出力される。
+    console.log('March'); //４月が２９日よりも小さいならばMarchと出力される。
 } else {
     console.log('April'); //それ以外ならばAprilが出力される。   
+}
+
+let mon = 4;// 月
+
+if (mon == 1) {
+    console.log('January');
+} else if (mon == 2) {
+    // 変数 mon ＝月に、数値 2 が代入されていた場合
+    console.log('February');// February を出力する
+} else if (mon == 3) {
+    console.log('March');
+} else {
+    // それ以外の場合
+    console.log('不明です');// 不明です を出力する
 }
